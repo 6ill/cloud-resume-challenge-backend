366670ff5405476ea47029b0f4eea75acf651253
6ill <kyrbill123@gmail.com>
2026-02-07 10:59:30 +0700

    feat: count unique visitors

--- visitor-counter/main.py
+++ visitor-counter/main.py
@@ -1,4 +1,6 @@
+import hashlib
 import json
+import time
 import boto3
 import os
 
@@ -8,19 +10,47 @@ table_name = os.environ["TABLE_NAME"]
 table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
-    response = table.update_item(
-        Key={
-            "id": "visitor_count"
-        },
-        UpdateExpression="SET counts = if_not_exists(counts, :start) + :inc",
-        ExpressionAttributeValues={
-            ":start": 0,
-            ":inc": 1
-        },
-        ReturnValues="UPDATED_NEW"
-    )
+    try:
+        ip_address = event["requestContext"]["http"]["sourceIp"]
+    except KeyError:
+        # Fallback for testing or non-proxy events
+        ip_address = "0.0.0.0"
 
-    new_count = response["Attributes"]["counts"]
+    hash_object = hashlib.sha256(ip_address.encode('utf-8'))
+    ip_hash = hash_object.hexdigest()
+
+    now = int(time.time())
+    ttl_expiry = now + (24 * 60 * 60)
+
+    try:
+        table.update_item(
+            Key={
+                "id": f"uv#{ip_hash}"
+            },
+            UpdateExpression="SET expires_at= :ttl",
+            ConditionExpression="attribute_not_exists(id)",
+            ExpressionAttributeValues={
+                ":ttl": ttl_expiry
+            }
+        )
+
+        response = table.update_item(
+            Key={
+                "id": "visitor_count"
+            },
+            UpdateExpression="SET counts = if_not_exists(counts, :start) + :inc",
+            ExpressionAttributeValues={
+                ":start": 0,
+                ":inc": 1
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+
+        new_count = response["Attributes"]["counts"]
+    
+    except boto3.client("dynamodb").exceptions.ConditionalCheckFailedException:
+        get_resp = table.get_item(Key={"id": "visitor_count"})
+        new_count = get_resp.get("Item", {}).get("counts", 0)
 
     return {
         "statusCode": 200,
